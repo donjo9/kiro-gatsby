20cd72754321c4491700fad0707076c838a75fac
Johnni Mortensen <don@jo9.dk>
2019-04-17 14:54:48 +0200

    Adds Fag Components

---
+++ src/components/Fag/fag.js
@@ -0,0 +1,30 @@
+import React from "react"
+import styled from "styled-components"
+
+const Fag = styled.div`
+   {
+    max-width: 1300px;
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+  }
+`
+
+const FagHeader = styled.div`
+   {
+    margin: 20px 10px;
+  }
+`
+
+const fag = props => {
+  return (
+    <Fag>
+      <FagHeader>
+        <h1>{props.headline}</h1>
+        <span>{props.desciption}</span>
+      </FagHeader>
+    </Fag>
+  )
+}
+
+export default fag
