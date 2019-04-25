da3a0453a5100b4df415cec7ee171628b4159054
Johnni Mortensen <don@jo9.dk>
2019-04-25 20:15:25 +0200

    Exporteret special components til boxes

---
+++ src/components/boxes.js
@@ -0,0 +1,47 @@
+
+import styled from "styled-components"
+
+export const Container = styled.section`
+   {
+    margin: 0px 15px;
+    background-color: var(--background);
+    box-shadow: 1px -1px 5px var(--highlight), -1px 1px 5px var(--highlight2);
+    border-radius: 10px;
+    padding: 15px;
+  }
+`
+export const Title = styled.div`
+   {
+    background-color: var(--background);
+    display: inline;
+    background-size: 1px 1em;
+    position: relative;
+    text-align: center;
+    &::after {
+      content: "";
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: -1px;
+      height: 3px;
+      text-align: center;
+      background: linear-gradient(
+        to right,
+        var(--background),
+        var(--highlight),
+        var(--background)
+      );
+    }
+    padding-bottom: 5px;
+    margin: 0;
+    font-size: 24px;
+    font-weight: 900;
+  }
+`
+
+export const Body = styled.div`
+   {
+    background-color: var(--background);
+    margin: 0;
+  }
+`
--- src/templates/specials-page.js
+++ src/templates/specials-page.js
@@ -1,52 +1,5 @@
 import React from "react"
-import styled from "styled-components"
-
-const Container = styled.section`
-   {
-    margin: 0px 15px;
-    background-color: var(--background);
-    box-shadow: 1px -1px 5px var(--highlight), -1px 1px 5px var(--highlight2);
-    border-radius: 10px;
-    padding: 15px;
-  }
-`
-const Title = styled.div`
-   {
-    background-color: var(--background);
-    display: inline;
-    background-size: 1px 1em;
-    position: relative;
-    &::after {
-      content: "";
-      width: 100%;
-      position: absolute;
-      left: 0;
-      bottom: -1px;
-      height: 3px;
-      text-align: center;
-      background: linear-gradient(
-        to right,
-        var(--background),
-        var(--highlight),
-        var(--background)
-      );
-    }
-
-    margin: 0;
-    padding: 15px;
-    font-size: 24px;
-    font-weight: 900;
-  }
-`
-
-const Body = styled.div`
-   {
-    background-color: var(--background);
-    padding: 15px;
-
-    margin: 0;
-  }
-`
+import {Title, Container, Body} from "../components/boxes"
 
 export default ({ pageContext }) => {
   return (
