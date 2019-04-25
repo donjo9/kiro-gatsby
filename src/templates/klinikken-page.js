3ceef2f42038f1fa6f19fa4a5c446adeeac8827d
Johnni Mortensen <don@jo9.dk>
2019-04-25 20:52:14 +0200

    Tilføjet data til klinikken

--- src/templates/klinikken-page.js
+++ src/templates/klinikken-page.js
@@ -1,17 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Container, Title, Body } from "../components/boxes"
 
 export default ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-  return <div>{frontmatter.title}</div>
+  const { Klinikken } = data
+
+  return (
+    <Container>
+      <Title>{Klinikken.frontmatter.overskrift}</Title>
+      <Body dangerouslySetInnerHTML={{ __html: Klinikken.html }} />
+    </Container>
+  )
 }
 
 export const pageQuery = graphql`
-  query OmPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "klinikken-page" } }) {
-      frontmatter {
-        title
-      }
-    }
+query KlinikkenPageTemplate {
+  Klinikken: markdownRemark(frontmatter: {templateKey: {eq: "klinikken-page"}}) {
+    frontmatter {overskrift}
+    html
   }
+}
 `
