44e571d2075d52cb0ed735d3641498c7993aba17
Johnni Mortensen <don@jo9.dk>
2019-04-26 11:50:07 +0200

    Tilføjer Meta tags og title til layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,60 @@
-import React from "react";
-import Footer from "../components/footer/footer";
-import Header from "../components/header/header";
-import "./layout.css";
+import React from "react"
+import Footer from "../components/footer/footer"
+import Header from "../components/header/header"
+import "./layout.css"
+import { Helmet } from "react-helmet"
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
+  const title = "Kiropraktorhuset"
+  return (
     <main>
-        <Header />
-        <section className="MainContent">
-        {children}
-        </section>
-        <Footer />
+      <Helmet
+        htmlAttributes={{
+          lang: "da",
+        }}
+        title={title}
+        meta={[
+          {
+            name: `description`,
+            content: `Specialister i bevægeapparatet  
+            ÅBNINGSTIDER  
+            Mandag kl. 08.00 - 18.00
+            Tirsdag kl. 08.00 - 18.00
+            Onsdag kl. 08.00 - 18.00
+            Torsdag kl. 08.00 - 18.00
+            Fredag kl. 08.00 - 18.00  
+            `,
+          },
+          {
+            property: `og:title`,
+            content: title,
+          },
+          {
+            property: `og:description`,
+            content: `Specialister i bevægeapparatet  
+            ÅBNINGSTIDER  
+            Mandag kl. 08.00 - 18.00
+            Tirsdag kl. 08.00 - 18.00
+            Onsdag kl. 08.00 - 18.00
+            Torsdag kl. 08.00 - 18.00
+            Fredag kl. 08.00 - 18.00  
+            `,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: "keywords",
+            content: "kiropraktor, fysioterapeut, massage",
+          },
+        ]}
+      />
+      <Header />
+      <section className="MainContent">{children}</section>
+      <Footer />
     </main>
-);
+  )
+}
 
-export default Layout;
+export default Layout
