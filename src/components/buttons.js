9ada0ff2672906ff68ad3d68d1c6b1d6401cd987
Johnni Mortensen <don@jo9.dk>
2019-05-01 19:39:02 +0200

    Tilføjer ExtLink med samme stil som Button

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,13 +1,16 @@
 import { Link } from "gatsby"
 import styled from "styled-components"
-
+const base = {
+  border: "solid 1px var(--highlight2)",
+  background: "var(--background)",
+  color: "white",
+  padding: "10px 20px",
+  borderRadius: "15px",
+  textAlign: "center",
+  textDecoration: "none",
+  boxShadow: "inset 0px 0px 4px var(--highlight2)",
+}
 export const Button = styled(Link)`
-  border: solid 1px var(--highlight2);
-  background: var(--background);
-  color: white;
-  padding: 10px 20px;
-  border-radius: 15px;
-  text-align: center;
-  text-decoration: none;
-  box-shadow: inset 0px 0px 4px var(--highlight2);
+  ${base}
 `
+export const ExtLink = styled.a(base)
