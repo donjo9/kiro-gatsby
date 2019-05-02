cb88dfe9f3555c8410dc37499dd747e07e730342
Johnni Mortensen <don@jo9.dk>
2019-05-02 12:12:38 +0200

    Tilføjer Facebook link i footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,6 +8,7 @@ const FooterContainer = styled.footer`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     justify-self: center;
+    align-items: center;
     width: 100%;
   }
 `
@@ -17,16 +18,42 @@ const FooterElement = styled.span`
     padding: 5px;
   }
 `
-
+const Facebook = styled.a`
+   {
+    background-color: rgb(66, 103, 178);
+    display: block;
+    text-align: center;
+    line-height: 1.2em;
+    font-size: 1.3rem;
+    margin: 0.2em;
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+    border-width: 1px;
+    border-color: rgb(66, 103, 178);
+    width: 1.2em;
+    height: 1.2em;
+    transition: background-color 500ms ease-in-out;
+    &:hover,
+    &:focus {
+      background-color: white;
+      color: rgb(66, 103, 178);
+    }
+  }
+`
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterElement>KiropraktorHuset</FooterElement>
+      <FooterElement>KiropraktorHuset Slagelse</FooterElement>
       <FooterElement>Sct. Mikkelsgade 4</FooterElement>
       <FooterElement>4200 Slagelse</FooterElement>
       <FooterElement>Tlf: 58 53 45 55</FooterElement>
       <FooterElement>mail@kiropraktorklinik.dk</FooterElement>
-      <FooterElement>© 2019 KiropraktorHuset</FooterElement>
+      <FooterElement>
+        <Facebook href="https://www.facebook.com/KiropraktorhusetSlagelse/"  target="_blank" rel="noopener noreferrer">
+          f
+        </Facebook>
+      </FooterElement>
     </FooterContainer>
   )
 }
