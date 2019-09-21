77480cbf78691cbd359c31332512ec7e7d827a4f
Johnni Mortensen <don@jo9.dk>
2019-09-21 13:30:33 +0200

    Fixer stillings opslag 0

--- src/templates/stillinger-page.js
+++ src/templates/stillinger-page.js
@@ -3,18 +3,22 @@ import { graphql } from "gatsby"
 import { Container, Body, Title, SubTitle } from "../components/boxes"
 
 const Stilinger = ({ data }) => {
-  const { Content } = data
+  let opslag = "Ingen stillinger pt."
+  if (data) {
+    const { Content } = data
 
-  const { frontmatter } = Content
-  const opslag = frontmatter.stilling.map(x => {
-    console.log(JSON.stringify(opslag, null, 2))
-    return (
-      <React.Fragment key={x.opslag.beskrivelse + x.opslag.overskrift}>
-        <SubTitle>{x.opslag.overskrift}</SubTitle>
-        <Body>{x.opslag.beskrivelse}</Body>
-      </React.Fragment>
-    )
-  })
+    const { frontmatter } = Content
+    const opslag = frontmatter.stilling.map(x => {
+      console.log(JSON.stringify(opslag, null, 2))
+      return (
+        <React.Fragment key={x.opslag.beskrivelse + x.opslag.overskrift}>
+          <SubTitle>{x.opslag.overskrift}</SubTitle>
+          <Body>{x.opslag.beskrivelse}</Body>
+        </React.Fragment>
+      )
+    })
+  }
+
   return (
     <Container>
       <Title>Stillinger:</Title>
